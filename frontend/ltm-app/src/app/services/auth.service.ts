d405179c2761dd138b088c06b2e106b819808ac3
Tiago dos Santos <tiago.talentfour@axismed.com.br>
2017-10-02 15:08:23 -0300

    Fixing dependencies

--- frontend/ltm-app/src/app/services/auth.service.ts
+++ frontend/ltm-app/src/app/services/auth.service.ts
@@ -39,7 +39,7 @@ export class AuthService {
 
     logout() {
         // remove user from local storage to log user out
-        localStorage.removeItem('currentUsers');
+        localStorage.removeItem('currentUser');
     }
 
 
